1ed7f65e81c19349292e80c7899aac0d33693fcb
jcrz <jfuentes.dev@gmail.com>
2020-05-11 19:47:13 -0400

    agregando bootstrap

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,6 @@
 INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (1,'Juan', 'Fuentes','jfuentes.dev@gmail.com','2020-05-11');
 INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (2,'Javiera', 'Mardones','pmardoness96@gmail.com','2020-05-13');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (3,'Carlos', 'Figueroa','c.figueroa91@gmail.com','2020-05-05');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (4,'Manuel', 'Muñoz','m_munoz@gmail.com','2020-04-21');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (5,'Estela', 'Sepulveda','estela.sepu18@gmail.com','2020-04-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (6,'Rodrigo', 'Gutierrez','rod.errez21@gmail.com','2020-04-16');
