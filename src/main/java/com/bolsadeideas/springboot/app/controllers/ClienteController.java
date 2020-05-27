a49c7898253364230255b10a7081f503e08e9fe1
jcrz <jfuentes.dev@gmail.com>
2020-05-26 22:43:04 -0400

    actualizacion

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,6 +32,19 @@ public class ClienteController {
 	
 	@Autowired
 	private IClienteService clienteService;
+	
+	@GetMapping(value="/ver/{id}")
+	public String ver(@PathVariable(value="id") Long id, Map<String, Object> model) {
+		
+		Cliente cliente = clienteService.findOne(id);
+		if(cliente == null) {
+			return "redirect:/listar";
+		}
+		model.put("cliente", cliente);
+		model.put("titulo", "Detalle cliente: " + cliente.getNombre());
+		return "ver";
+	}
+	
 	
 	@RequestMapping(value="/listar", method=RequestMethod.GET)
 	public String listar(Model model){
